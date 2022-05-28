2ed6ed750499c9b77e18cc4bc246a1479b65357a
Simon Gasse <sgasse@users.noreply.github.com>
2022-05-28 12:11:26 +0200

    Creating hash

--- src/hotel/mod.rs
+++ src/hotel/mod.rs
@@ -1,22 +1,16 @@
-use async_graphql::{ComplexObject, Context, EmptySubscription, Object, Schema, SimpleObject};
+use async_graphql::{Context, EmptySubscription, Object, Schema, SimpleObject};
+use base64ct::{Base64, Encoding};
 use mongodb::Client;
 use serde::{Deserialize, Serialize};
-
+use sha2::{Digest, Sha256};
 pub type HotelSchema = Schema<QueryRoot, MutationRoot, EmptySubscription>;
 
 #[derive(SimpleObject, Serialize, Deserialize)]
-#[graphql(complex)]
 pub struct Guest {
     first_name: String,
     last_name: String,
     date_of_birth: String,
-}
-
-#[ComplexObject]
-impl Guest {
-    async fn guest_hash(&self) -> &str {
-        "the_hash".into()
-    }
+    uuid: String,
 }
 
 pub struct QueryRoot;
@@ -36,6 +30,7 @@ impl QueryRoot {
             first_name: "John".into(),
             last_name: "Doe".into(),
             date_of_birth: "1991-01-01".into(),
+            uuid: "the_uuid".into(),
         }]
     }
 }
@@ -51,17 +46,29 @@ impl MutationRoot {
         last_name: String,
         date_of_birth: String,
     ) -> String {
+        let uuid = guest_hash(&first_name, &last_name, &date_of_birth);
         let new_guest = Guest {
             first_name,
             last_name,
             date_of_birth,
+            uuid,
         };
         let db = ctx.data::<Client>().expect("DB Client").database("hotel");
         let guests = db.collection::<Guest>("guests");
         guests
-            .insert_one(new_guest, None)
+            .insert_one(&new_guest, None)
             .await
             .expect("Insert doc");
-        "guest_hash".into()
+        new_guest.uuid
     }
+}
+
+fn guest_hash(first_name: &str, last_name: &str, date_of_birth: &str) -> String {
+    let mut hasher = Sha256::new();
+    hasher.update(first_name);
+    hasher.update(last_name);
+    hasher.update(date_of_birth);
+    let hash = hasher.finalize();
+
+    Base64::encode_string(&hash)
 }
