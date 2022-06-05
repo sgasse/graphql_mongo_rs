1187c29978022f13e72c4a3370e31d26004865d0
Simon Gasse <sgasse@users.noreply.github.com>
2022-06-05 09:43:36 +0200

    Make mongo and bind address configurable

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -4,9 +4,60 @@ use async_graphql::{
     Schema,
 };
 use async_graphql_actix_web::{GraphQLRequest, GraphQLResponse, GraphQLSubscription};
+use clap::Parser;
 use gql_server_rs::model::{HotelSchema, MutationRoot, QueryRoot, SubscriptionRoot};
 use mongodb::{options::ClientOptions, Client};
 use type_pubsub::TypePubSub;
+
+#[derive(Parser, Debug)]
+#[clap(author, version, about)]
+struct Args {
+    /// MongoDB host
+    #[clap(long, default_value = "localhost")]
+    mongo_host: String,
+
+    /// MongoDB port
+    #[clap(long, default_value_t = 27017)]
+    mongo_port: i32,
+
+    /// GraphQL bind address
+    #[clap(long, default_value = "127.0.0.1")]
+    gql_bind_address: String,
+
+    /// GraphQL bind port
+    #[clap(long, default_value_t = 8080)]
+    gql_bind_port: i32,
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    let args = Args::parse();
+    let mongo_url = &format!("mongodb://{}:{}", args.mongo_host, args.mongo_port);
+    let playground_address = &format!("{}:{}", args.gql_bind_address, args.gql_bind_port);
+
+    let schema = Schema::build(QueryRoot, MutationRoot, SubscriptionRoot)
+        .data(db_client(mongo_url).await)
+        .data(TypePubSub::default())
+        .finish();
+
+    println!("Playground at: http://{}", playground_address);
+
+    HttpServer::new(move || {
+        App::new()
+            .app_data(web::Data::new(schema.clone()))
+            .service(web::resource("/").guard(guard::Post()).to(index))
+            .service(
+                web::resource("/")
+                    .guard(guard::Get())
+                    .guard(guard::Header("upgrade", "websocket"))
+                    .to(index_ws),
+            )
+            .service(web::resource("/").guard(guard::Get()).to(index_playground))
+    })
+    .bind(playground_address)?
+    .run()
+    .await
+}
 
 async fn index(schema: web::Data<HotelSchema>, req: GraphQLRequest) -> GraphQLResponse {
     schema.execute(req.into_inner()).await.into()
@@ -51,33 +102,4 @@ async fn db_client(mongo_url: &str) -> Client {
     }
 
     client
-}
-
-#[actix_web::main]
-async fn main() -> std::io::Result<()> {
-    let mongo_url = "mongodb://localhost:27017";
-    let playground_address = "127.0.0.1:8080";
-
-    let schema = Schema::build(QueryRoot, MutationRoot, SubscriptionRoot)
-        .data(db_client(mongo_url).await)
-        .data(TypePubSub::default())
-        .finish();
-
-    println!("Playground at: http://{}", playground_address);
-
-    HttpServer::new(move || {
-        App::new()
-            .app_data(web::Data::new(schema.clone()))
-            .service(web::resource("/").guard(guard::Post()).to(index))
-            .service(
-                web::resource("/")
-                    .guard(guard::Get())
-                    .guard(guard::Header("upgrade", "websocket"))
-                    .to(index_ws),
-            )
-            .service(web::resource("/").guard(guard::Get()).to(index_playground))
-    })
-    .bind(playground_address)?
-    .run()
-    .await
 }
